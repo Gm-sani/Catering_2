560342afe62433151c83630753bcea25ae244818
Gmsani <naeemmurtaza25@gmail.com>
2024-02-12 03:14:26 +0500

    second commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router';
 
 export default function Navbar() {
+  const navigate =useNavigate();
   return (
     <div>
      <div className='h-[7vh] bg-slate-400 text-black p-3 flex justify-between font-serif'>
         <div>
-            <p>Catering</p>
+            <button onClick={()=>{navigate("/")}} >Catering</button>
         </div>
         <div className='flex gap-5'>
-            <p>About</p>
-            <p>Menu</p>
-            <p>Contact</p>
+            <button onClick={()=>{navigate("about")}} >About</button>
+            <button onClick={()=>{navigate("menu")}} >Menu</button>
+            <button onClick={()=>{navigate("cont")}} >Contact</button>
+            
         </div>
      </div>
     </div>
